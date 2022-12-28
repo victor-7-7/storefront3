6ca7425590d4e59f6cbbf76bb51d3e5aed225863
victor-7-7 <Victor.7.7@ya.ru>
2022-12-28 09:30:10 +0700

    Before docker

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -3,6 +3,7 @@ from .common import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'v=1w9+tzg)m=9t2&%=+7-l17c3a)jfjv=qmecgld#^rd!0!f4a'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vvidom.ru']
